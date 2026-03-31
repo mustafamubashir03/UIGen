7bf6d5cabb43ad76880dcb93da3b8b20575ecaee
Mustafa <mustafamubashir87@gmail.com>
2026-03-31 16:43:13 +0500

    Placed inngest functions inside inngest

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,6 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import { codeAgentFunction } from "./functions";
+
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
